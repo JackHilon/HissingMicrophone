d5c225c1f6d41e834135b806318bb53a7bb1bf6c
markandmark1111@gmail.com <markandmark1111@gmail.com>
2020-06-25 17:48:09 +0200

    just fix code

--- HissingMicrophone/Program.cs
+++ HissingMicrophone/Program.cs
@@ -9,16 +9,45 @@ namespace HissingMicrophone
             // Hissing Microphone
             // https://open.kattis.com/problems/hissingmicrophone 
             // two successive chars compare in a string
-            while (true)
-            {
-                Console.WriteLine(MyStringCheck(Console.ReadLine()));
-            }
+
+            Console.WriteLine(MyStringCheck(EnterYourString()));
         }
+        // ==============================================================================
         private static string MyStringCheck(string str)
         {
             if (str.Contains("ss"))
                 return "hiss";
             else return "no hiss";
+        }
+        // ==============================================================================
+
+        private static string EnterYourString()
+        {
+            string str = "";
+            try
+            {
+                str = Console.ReadLine();
+                if (MyStringConditions(str) == false)
+                    throw new FormatException();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return EnterYourString();
+            }
+            return str;
+        }
+
+        private static bool MyStringConditions(string str)
+        {
+            if (str.Length < 1 || str.Length > 30)
+                return false;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsLower(str[i]) == false)
+                    return false;
+            }
+            return true;
         }
     }
 }
