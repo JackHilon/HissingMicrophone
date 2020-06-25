b8503d036d8ca45bcefe0cbbee3a78f7f99a882e
markandmark1111@gmail.com <markandmark1111@gmail.com>
2020-06-25 17:26:29 +0200

    Add project files.

---
+++ HissingMicrophone/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HissingMicrophone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hissing Microphone
+            // https://open.kattis.com/problems/hissingmicrophone 
+            // two successive chars compare in a string
+            while (true)
+            {
+                Console.WriteLine(MyStringCheck(Console.ReadLine()));
+            }
+        }
+        private static string MyStringCheck(string str)
+        {
+            if (str.Contains("ss"))
+                return "hiss";
+            else return "no hiss";
+        }
+    }
+}
